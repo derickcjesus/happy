ac013b4e0e8781f5d4dc851e91d7ace65b361ef3
Derick de Jesus <derickcjesus@gmail.com>
2020-10-15 17:13:03 -0300

    feat: function to select images

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -21,6 +21,29 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-
 //create and add marker
 L.marker([-2.5606303,-44.3281628], { icon }).addTo(map)
+
+//image gallery
+function selectImage(event){
+    
+    const button = event.currentTarget;
+    
+    // remove all .actives classes 
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+    
+    function removeActiveClass(button){
+        button.classList.remove("active")
+    }
+
+    //select a image clicked
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //att image container 
+    imageContainer.src = image.src
+
+    // add .active class for this button 
+    button.classList.add("active")
+}
